8a93cd3010788126baefbc23903d0b7a167ed3a0
liuwenzhu <547485261@qq.com>
2021-02-25 18:26:10 +0800

    opts:ts

--- ts/ts描述文件/模块代码/src/jquery.d.ts
+++ ts/ts描述文件/模块代码/src/jquery.d.ts
@@ -1,2 +1,16 @@
 // Es6 模块化
-declare module 'jquery'
+declare module 'jquery' {
+    interface JqueryInstance {
+      html: (html: string) => JqueryInstance;
+    }
+    // 混合类型
+    function $(readyFunc: () => void): void;
+    function $(selector: string): JqueryInstance;
+    namespace $ {
+      namespace fn {
+        class init {}
+      }
+    }
+    export = $;
+  }
+  
---
+++ ts/ts装饰器/1.类的装饰器/index.ts
@@ -0,0 +1,21 @@
+function testDecorator() {
+    return function <T extends new (...args: any[]) => any>(constructor: T) {
+        return class extends constructor {
+            name = 'lee';
+            getName() {
+                return this.name;
+            }
+        };
+    };
+}
+
+@testDecorator()
+class Test {
+    name: string;
+    constructor(name: string) {
+        this.name = name;
+    }
+};
+
+const test = new Test('dell');
+console.log((test as any).getName());
